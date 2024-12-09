8fd510d49f8e731871306fb93fc1ff5096b4018a
koyasoo <tmkoyama312@gmail.com>
2024-12-09 15:18:36 +0900

    Update: hammerSpoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,65 +1,63 @@
-
 hs.window.animationDuration = 0
-units = {
-  right       = { x = 0.50, y = 0.00, w = 0.50, h = 1.00 },
-  left        = { x = 0.00, y = 0.00, w = 0.50, h = 1.00 },
-  top         = { x = 0.00, y = 0.00, w = 1.00, h = 1.00 },
-  bot         = { x = 0.00, y = 0.50, w = 1.00, h = 0.50 },
-  sright      = { x = 0.70, y = 0.00, w = 0.30, h = 1.00 },
-  sleft       = { x = 0.00, y = 0.00, w = 0.70, h = 1.00 },
-  stop        = { x = 0.70, y = 0.00, w = 0.30, h = 0.50 },
-  sbot        = { x = 0.70, y = 0.50, w = 0.30, h = 0.50 }
+
+layoutUnits = {
+  rightHalf = { x = 0.50, y = 0.00, w = 0.50, h = 1.00 },
+  leftHalf = { x = 0.00, y = 0.00, w = 0.50, h = 1.00 },
+  fullScreen = { x = 0.00, y = 0.00, w = 1.00, h = 1.00 },
+  rightThird = { x = 0.70, y = 0.00, w = 0.30, h = 1.00 },
+  leftTwoThirds = { x = 0.00, y = 0.00, w = 0.70, h = 1.00 },
+  topRightQuarter = { x = 0.70, y = 0.00, w = 0.30, h = 0.50 },
+  bottomRightQuarter = { x = 0.70, y = 0.50, w = 0.30, h = 0.50 }
 }
 
-mash = { 'ctrl', 'command' }
-hs.hotkey.bind(mash, 'right', function() 
-  hs.window.focusedWindow():move(units.right, nil, true) 
+function bindHotKey(modifiers, key, action)
+  hs.hotkey.bind(modifiers, key, action)
 end
-)
-hs.hotkey.bind(mash, 'left', function() 
-  hs.window.focusedWindow():move(units.left, nil, true) 
-end
-)
-hs.hotkey.bind(mash, 'up', function() 
-  hs.window.focusedWindow():move(units.top, nil, true) 
-end
-)
-hs.hotkey.bind(mash, 'down', function() 
-  hs.window.focusedWindow():move(units.bot, nil, true) 
-end
-)
-mash = { 'ctrl', 'shift', 'command' }
-hs.hotkey.bind(mash, 'right', function() 
-  hs.window.focusedWindow():move(units.sright, nil, true) 
-end
-)
-hs.hotkey.bind(mash, 'left', function() 
-  hs.window.focusedWindow():move(units.sleft, nil, true) 
-end
-)
-hs.hotkey.bind(mash, 'up', function() 
-  hs.window.focusedWindow():move(units.stop, nil, true) 
-end
-)
-hs.hotkey.bind(mash, 'down', function() 
-  hs.window.focusedWindow():move(units.sbot, nil, true) 
-end
-)
-hs.hotkey.bind({'ctrl'}, 'q', function()
-  local warp = hs.application.find('Warp')
-  if warp:isFrontmost() then
-    warp:hide()
-  else
-    hs.application.launchOrFocus('/Applications/Warp.app')
+
+function moveTo(unit)
+  return function()
+    hs.window.focusedWindow():move(unit, nil, true)
   end
 end
-)
-hs.hotkey.bind({'ctrl'}, 'e', function()
-  local finder = hs.application.find('Finder')
-  if finder:isFrontmost() then
-    finder:hide()
-  else
-    hs.application.launchOrFocus('/System/Library/CoreServices/Finder.app')
+
+function moveToBottomHalfKeepingWidth()
+  local win = hs.window.focusedWindow()
+  local frame = win:frame()
+  local screenFrame = win:screen():frame()
+  win:setFrame({
+    x = frame.x,
+    y = screenFrame.y + screenFrame.h * 0.50,
+    w = frame.w,
+    h = screenFrame.h * 0.50
+  }, 0)
+end
+
+function toggleAppVisibility(appName, appPath)
+  return function()
+    local app = hs.application.find(appName)
+    if app and app:isFrontmost() then
+      app:hide()
+    else
+      hs.application.launchOrFocus(appPath)
+    end
   end
 end
-)
+
+hotKeyBindings = {
+  { { 'ctrl', 'command' }, 'right', moveTo(layoutUnits.rightHalf) },
+  { { 'ctrl', 'command' }, 'left', moveTo(layoutUnits.leftHalf) },
+  { { 'ctrl', 'command' }, 'up', moveTo(layoutUnits.fullScreen) },
+  { { 'ctrl', 'command' }, 'down', moveToBottomHalfKeepingWidth },
+
+  { { 'ctrl', 'shift', 'command' }, 'right', moveTo(layoutUnits.rightThird) },
+  { { 'ctrl', 'shift', 'command' }, 'left', moveTo(layoutUnits.leftTwoThirds) },
+  { { 'ctrl', 'shift', 'command' }, 'up', moveTo(layoutUnits.topRightQuarter) },
+  { { 'ctrl', 'shift', 'command' }, 'down', moveTo(layoutUnits.bottomRightQuarter) },
+
+  { { 'ctrl' }, 'q', toggleAppVisibility('Warp', '/Applications/Warp.app') },
+  { { 'ctrl' }, 'e', toggleAppVisibility('Finder', '/System/Library/CoreServices/Finder.app') }
+}
+
+for _, binding in ipairs(hotKeyBindings) do
+  bindHotKey(binding[1], binding[2], binding[3])
+end
